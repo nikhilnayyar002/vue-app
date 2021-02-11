6cb55277a716d4f1b87fe9724d538132aab07404
Nikhil Nayyar <nikhilnayyar002@gmail.com>
2021-02-11 13:07:05 +0530

    Add empty components

--- src/components/main/App.vue
+++ src/components/main/App.vue
@@ -1,18 +1,15 @@
 <template>
   <img alt="Vue logo" src="../../assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 /** import index style */
 import "../../styles/index.scss";
 /** */
-import HelloWorld from "./HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
   },
 };
 </script>
